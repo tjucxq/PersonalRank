0c0aa01d6021867ad0f7aede01864b6967fba683
tjucxq <747106471@qq.com>
2017-01-12 16:34:28 +0800

    haven't done

---
+++ PersonalRank.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+
+def PersonalRank(G, alpha, root, max_step):
+    rank = dict()
+    rank = {x:0 for x in G.keys()}
+    rank[root] = 1
+    #start iteration
+    for k in range(max_step):
+        tmp = {x:0 for x in G.keys()}
