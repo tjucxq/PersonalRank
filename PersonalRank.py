be35ccee3b8fc379ba6f6b9c5fea48bbaffbd31a
tjucxq <747106471@qq.com>
2017-01-12 16:43:58 +0800

    haven't done

--- PersonalRank.py
+++ PersonalRank.py
@@ -7,3 +7,5 @@ def PersonalRank(G, alpha, root, max_step):
     #start iteration
     for k in range(max_step):
         tmp = {x:0 for x in G.keys()}
+        #get node i and i's out edges set ri
+        for i, ri in G.items():
